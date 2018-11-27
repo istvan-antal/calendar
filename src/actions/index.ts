c566fed232566198397caf08dd3a5542ca08143d
István Antal <i@istvan.xyz>
2018-11-27 19:35:29 +0000

    Remove example

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,34 +17,3 @@ export function createAction<T extends string, P>(type: T, payload?: P) {
 type FunctionType = (...args: any[]) => any;
 interface ActionCreatorMapObject { [actionCreator: string]: FunctionType; }
 export type ActionsUnion<A extends ActionCreatorMapObject> = ReturnType<A[keyof A]>;
-
-// Example actions/counter.ts
-/*
-import { createAction, ActionsUnion } from '.';
-
-export const counterActions = {
-    reset: () => createAction('reset'),
-    increment: (amount = 1) => createAction('increment', amount),
-    decrement: (amount = 1) => createAction('decrement', amount),
-}
-
-export type CounterActions = ActionsUnion<typeof counterActions>;
-*/
-
-// Example reducers/counter.ts
-/*
-import { CounterActions } from '../actions/counter';
-
-export const counter = (state = 0, action: CounterActions) => {
-    switch (action.type) {
-    case 'reset':
-        return 0;
-    case 'increment':
-        return state + action.data;
-    case 'decrement':
-        return state - action.data;
-    default:
-        return state;
-    }
-}
-*/
