a09ede3d550374bf49c408daf4d831da56ab39ee
István Antal <i@istvan.xyz>
2018-11-27 19:58:11 +0000

    Add vscode settings

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,8 +6,6 @@ import { serverFormMiddleware } from './middleware/serverForm';
 // tslint:disable-next-line:no-any
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// export type State = ReturnType<typeof reducers>;
-
 const store = createStore(
     reducers,
     composeEnhancers(
