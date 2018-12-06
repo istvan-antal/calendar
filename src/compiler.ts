9f0258c53af80f6ea54f5dd0aac8168090cd34ec
István Antal <i@istvan.xyz>
2018-12-06 21:35:06 +0000

    Add electron runner

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,7 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 import data from './data';
 
 // tslint:disable-next-line:no-any arrow-return-shorthand
 export default (config: any) => {
+    config.entry = {
+        app: config.entry.length > 1 ? config.entry : config.entry[0],
+        desktop: config.entry.length > 1 ? [config.entry[0], './src/desktop'] : './src/desktop',
+    };
+
+    config.output.filename = '[name].js';
+
+    config.plugins = [
+        ...config.plugins,
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './src/index.html',
+            filename: 'index.html',
+            chunks: ['app', 'commons'],
+        }),
+    ];
+
+    config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: 'commons',
+                    chunks: 'initial',
+                    minChunks: 2,
+                    minSize: 0,
+                },
+            },
+        },
+        occurrenceOrder: true, // To keep filename consistent between
+        // different modes (for example building only)
+    };
+
     return {
         ...config,
         devServer: {
